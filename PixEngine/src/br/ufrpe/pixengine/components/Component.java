fec7d39db2997c40ab004405ca866b955fd0992b
lmarques7 <leandro.marques@gmail.com>
2016-05-04 13:23:18 -0300

    OK

--- PixEngine/src/br/ufrpe/pixengine/components/Component.java
+++ PixEngine/src/br/ufrpe/pixengine/components/Component.java
@@ -4,14 +4,8 @@ import br.ufrpe.pixengine.core.GameContainer;
 import br.ufrpe.pixengine.core.Renderer;
 
 public abstract class Component {
-<<<<<<< HEAD
-	protected String tag = null; 
-=======
 	protected String tag = null;
->>>>>>> branch 'master' of https://github.com/lmarques7/PixEngine.git
-
 	public abstract void update(GameContainer gc, GameObject object, float dt);
-
 	public abstract void render(GameContainer gc, Renderer r);
 
 	public String getTag() {
